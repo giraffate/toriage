19485b05636677597556346e3154f5ca2f44cb5f
Takayuki Nakata <f.seasons017@gmail.com>
2021-02-12 16:55:43 +0900

    hello tide and tera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use http_types::Body;
+use std::str::FromStr;
+use tera::Tera;
+use tide::http::Mime;
+use tide::Request;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let tera = Tera::new("templates/**/*")?;
+
+    let mut app = tide::with_state(tera);
+    app.at("/:name").get(hello);
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn hello(req: Request<Tera>) -> tide::Result {
+    let tera = req.state();
+    let mut response = tide::Response::new(200);
+    let mut context = tera::Context::new();
+    let name: String = req.param("name")?.to_string();
+    context.insert("name", &name);
+
+    let mut body = Body::from_string(tera.render("hello.html", &context)?);
+
+    let mime = Mime::from_str("text/html;charset=utf-8").unwrap();
+    body.set_mime(mime);
+    response.set_body(body);
+
+    Ok(response)
 }
