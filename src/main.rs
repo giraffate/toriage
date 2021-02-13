52ceb5663ec6f29238b512a7775886319bdc33fe
Takayuki Nakata <f.seasons017@gmail.com>
2021-02-13 23:47:49 +0900

    Update style

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{Duration, Utc};
 use http_types::Body;
 use serde::Serialize;
 use serde_json::value::{to_value, Value};
@@ -28,10 +29,45 @@ async fn pulls(req: Request<Tera>) -> tide::Result {
     let base_pulls = page.take_items();
     let mut pulls: Vec<Value> = Vec::new();
     for base_pull in base_pulls.into_iter() {
+        let assignee = if base_pull.assignee.is_some() {
+            base_pull.assignee.unwrap().login
+        } else {
+            "".to_string()
+        };
+        let updated_at = if base_pull.updated_at.is_some() {
+            base_pull.updated_at.unwrap().to_rfc2822()
+        } else {
+            "".to_string()
+        };
+        let need_triage = if base_pull.updated_at.is_none() {
+            "lightgreen".to_string()
+        } else {
+            let yellow_line = Utc::now() - Duration::days(14);
+            let red_line = Utc::now() - Duration::days(28);
+            let updated_at = base_pull.updated_at.unwrap();
+
+            if updated_at <= red_line {
+                "red".to_string()
+            } else if updated_at <= yellow_line {
+                "yellow".to_string()
+            } else {
+                "lightgreen".to_string()
+            }
+        };
+        let labels = if base_pull.labels.is_empty() {
+            vec![""]
+        } else {
+            base_pull.labels.map(|label| label.name)
+        };
+
         let pull = PullRequest {
             html_url: base_pull.html_url,
             number: base_pull.number,
             title: base_pull.title,
+            assignee: assignee,
+            updated_at: updated_at,
+            need_triage: need_triage,
+            labels: labels,
         };
         pulls.push(to_value(pull)?);
     }
@@ -54,4 +90,8 @@ struct PullRequest {
     pub html_url: String,
     pub number: u64,
     pub title: String,
+    pub assignee: String,
+    pub updated_at: String,
+    pub need_triage: String,
+    pub labels: Vec<String>,
 }
