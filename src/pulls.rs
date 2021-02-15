ff28c8387914224625c4b96a55d4e4e40f9c9f82
Takayuki Nakata <f.seasons017@gmail.com>
2021-02-15 22:55:37 +0900

    Refactoring

--- src/pulls.rs
+++ src/pulls.rs
@@ -18,32 +18,17 @@ pub async fn pulls(req: Request<Tera>) -> tide::Result {
     let base_pulls = page.take_items();
     let mut pulls: Vec<Value> = Vec::new();
     for base_pull in base_pulls.into_iter() {
-        let assignee = if base_pull.assignee.is_some() {
-            base_pull.assignee.unwrap().login
-        } else {
-            "".to_string()
-        };
+        let assignee = base_pull.assignee.map_or("".to_string(), |v| v.login);
+        let updated_at = base_pull
+            .updated_at
+            .map_or("".to_string(), |v| v.to_rfc2822());
 
-        let updated_at = if base_pull.updated_at.is_some() {
-            base_pull.updated_at.unwrap().to_rfc2822()
-        } else {
-            "".to_string()
-        };
-
-        let need_triage = if base_pull.updated_at.is_none() {
-            "lightgreen".to_string()
-        } else {
-            let yellow_line = Utc::now() - Duration::days(14);
-            let red_line = Utc::now() - Duration::days(28);
-            let updated_at = base_pull.updated_at.unwrap();
-
-            if updated_at <= red_line {
-                "red".to_string()
-            } else if updated_at <= yellow_line {
-                "yellow".to_string()
-            } else {
-                "lightgreen".to_string()
-            }
+        let yellow_line = Utc::now() - Duration::days(14);
+        let red_line = Utc::now() - Duration::days(28);
+        let need_triage = match base_pull.updated_at {
+            Some(updated_at) if updated_at <= red_line => "red".to_string(),
+            Some(updated_at) if updated_at <= yellow_line => "yellow".to_string(),
+            _ => "lightgreen".to_string(),
         };
 
         let labels = if base_pull.labels.is_empty() {
@@ -61,13 +46,14 @@ pub async fn pulls(req: Request<Tera>) -> tide::Result {
             html_url: base_pull.html_url,
             number: base_pull.number,
             title: base_pull.title,
-            assignee: assignee,
-            updated_at: updated_at,
-            need_triage: need_triage,
-            labels: labels,
+            assignee,
+            updated_at,
+            need_triage,
+            labels,
         };
         pulls.push(to_value(pull)?);
     }
+
     let mut context = tera::Context::new();
     context.insert("pulls", &pulls);
     context.insert("owner", &owner);
