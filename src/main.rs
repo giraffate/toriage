7c8a6824ac74b3d17c30b2ac53ef2c6053156875
Takayuki Nakata <f.seasons017@gmail.com>
2021-02-19 23:50:47 +0900

    Add 404

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod pulls;
 
 use tera::Tera;
+use tide::http::mime;
+use tide::utils::After;
+use tide::{Response, StatusCode};
 
 use crate::pulls::pulls;
 
@@ -10,6 +13,19 @@ async fn main() -> tide::Result<()> {
     let tera = Tera::new("templates/**/*")?;
 
     let mut app = tide::with_state(tera);
+
+    app.with(After(|response: Response| async move {
+        let response = match response.status() {
+            StatusCode::NotFound => Response::builder(404)
+                .content_type(mime::HTML)
+                .body(include_str!("../templates/404.html"))
+                .build(),
+            _ => response,
+        };
+
+        Ok(response)
+    }));
+
     app.at("/pulls/:owner/:repo").get(pulls);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
